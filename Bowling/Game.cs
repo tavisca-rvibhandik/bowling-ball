1faadfa7f98e7a515586cc452d6f0a78af77a7e3
rashmi <rvibhandik@tavisca.com>
2014-07-15 18:49:38 +0530

    This is the Bowling Test Cases by Rashmi

--- Bowling/Game.cs
+++ Bowling/Game.cs
@@ -8,15 +8,63 @@ namespace Bowling
 {
     public class Game
     {
+        public int FinalScore = 0;
+        public int[] rolls = new int[23];
+
+        public int cnt = 0;
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            rolls[cnt] = pins;
+            if (cnt % 2 == 0 && pins == 10)                 //placing values in array per roll
+            {
+                if (cnt == 20)
+                {
+                    cnt++;
+                }
+                if (cnt == 21)
+                {}
+                else
+                {
+                    rolls[cnt + 1] = 0;
+                    cnt = cnt + 1;
+                }
+            }
+            else
+            {
+                cnt++;
+            }
+            
         }
 
-        public int GetScore()
+        public int GetScore()                        //calculating intermediate score
         {
-            throw new NotImplementedException();
+             FinalScore = FinalScore + rolls[cnt] + rolls[cnt + 1];
+             return FinalScore;
         }
 
-    }
+        public int GetFinalScore()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                if (i % 2 == 0 && rolls[i] == 10)               //calculating score for strikes
+                {
+                    FinalScore = FinalScore + rolls[i] + rolls[i+2];
+                    if (rolls[i+2] == 10)
+                        FinalScore = FinalScore + rolls[i + 4];
+                    FinalScore = FinalScore + rolls[i+3];
+                }
+
+                else if (rolls[i] + rolls[i+1] == 10)           //calculating score for spares
+                {
+                    FinalScore = FinalScore + rolls[i] + rolls[i + 1] + rolls[i + 2];
+                }
+                else
+                {
+                    FinalScore = FinalScore + rolls[i] + rolls[i + 1];
+                }
+                i = i + 1;
+            }
+           return FinalScore;
+        }
+     }  
 }
--- BowlingFixtures/GameFixture.cs
+++ BowlingFixtures/GameFixture.cs
@@ -1,14 +1,81 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bowling;
 
 namespace BowlingFixtures
 {
     [TestClass]
     public class GameFixture
     {
+         [TestInitialize]
+        public void First(){
+          
+         }
+
+         [TestMethod]
+         public void AllAreSpares()                 //Test for all strikes
+         {
+             Game game = new Game();
+             for (int i = 0; i < 10; i++)
+             
+                 game.Roll(1);
+                 game.Roll(9);
+             if (game.cnt == 20)
+                 Assert.AreEqual(game.GetFinalScore(), 200);
+         }
+
         [TestMethod]
-        public void TestMethod1()
+        public void AllAreStrikes()                 //Test for all strikes
         {
+            Game game = new Game();
+            for (int i = 0; i < 10; i++)
+                game.Roll(10);
+            if (game.cnt == 20)
+               
+            Assert.AreEqual(game.GetFinalScore(), 300);
         }
+
+        [TestMethod]
+        public void AllAreGutters()                 //Test for all strikes
+        {
+            Game game = new Game();
+            for (int i = 0; i < 20; i++)
+                game.Roll(0);
+            Assert.AreEqual(game.GetFinalScore(), 0);
+        }
+
+        [TestMethod]
+        public void IncorrectScoreException()           //Calculating total score
+        {
+            Game game = new Game();
+            for (int i = 0; i < 5; i++)
+            game.Roll(3);
+            game.Roll(5);
+            game.Roll(6);
+            game.Roll(2);
+            if (game.GetFinalScore() > 300)
+                Assert.Fail("Incorrect Score.......");
+        }
+
+        [TestMethod]
+        public void GameOverException()                 //Limiting number of rolls
+        {
+            Game game = new Game();
+            if(game.cnt>21)
+            {
+                Assert.Fail("Game over....");
+            }
+        }
+
+         [TestMethod]
+         public void IncorrectFrameScore()              //Calculating score per frame
+         {
+             Game game = new Game();
+             game.Roll(4);
+             game.Roll(5);
+                 
+                 if (game.GetScore()>10)
+                     Assert.Fail("Incorrect Frame Score.......");
+         }
     }
 }
